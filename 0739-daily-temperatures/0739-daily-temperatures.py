ac09276aa6ff9be2318683c93dfb1f81c399f553
Bhargav-Kumar2007 <bhargavkr2007@gmail.com>
2026-09-17 01:12:50 +0530

    Time: 231 ms (5.12%), Space: 44.8 MB (5.61%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -10,15 +10,13 @@ class Solution:
         for i in range(ln):
             n+=1
             mx=0
-            for j in range(-1,-lsn-1,-1):
-                if temperatures[ls[j][0]]<temperatures[i]:
-                    mx+=1
+            while ls:
+                if temperatures[ls[-1][0]]<temperatures[i]:
+                    a=ls.pop()
+                    sd[a[0]]=n-a[1]
+                    lsn-=1
                 else:
                     break
-            for j in range(mx):
-                a=ls.pop()
-                lsn-=1
-                sd[a[0]]=n-a[1]
             ls.append((i,n))
             lsn+=1
         res=[]
