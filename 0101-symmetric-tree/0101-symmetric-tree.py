d4459ac6796dc38fc02ab6c843c7081d71f9622b
Bhargav-Kumar2007 <bhargavkr2007@gmail.com>
2026-09-21 01:58:00 +0530

    Time: 0 ms (100%), Space: 19.5 MB (19.25%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -13,15 +13,7 @@ class Solution:
                 return False
             if p.val != q.val:
                 return False
-            return sym(p.left,q.left) and sym(p.right,q.right)
-        def rev(n):
-            n.left,n.right=n.right,n.left
-            if n.left:
-                rev(n.left)
-            if n.right:
-                rev(n.right)
+            return sym(p.left,q.right) and sym(p.right,q.left)
         if not root:
             return True
-        if root.left:
-            rev(root.left)
         return sym(root.left,root.right)
